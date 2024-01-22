1d1987c916c407d79138d3658c2bcf1e7d966227
Ihor_Ostapovych <iostapovychweb@gmail.com>
2024-01-22 15:32:21 +0200

    docs: updated

--- docs/docs.go
+++ docs/docs.go
@@ -16,7 +16,44 @@ const docTemplate = `{
     },
     "host": "{{.Host}}",
     "basePath": "{{.BasePath}}",
-    "paths": {}
+    "paths": {
+        "/api/v1/table/{sheet}/{cell}": {
+            "post": {
+                "description": "Add a new pet to the store",
+                "consumes": [
+                    "application/json"
+                ],
+                "produces": [
+                    "application/json"
+                ],
+                "summary": "adds value to cell",
+                "parameters": [
+                    {
+                        "type": "string",
+                        "description": "sheet name",
+                        "name": "sheet",
+                        "in": "path",
+                        "required": true
+                    },
+                    {
+                        "type": "string",
+                        "description": "cell address",
+                        "name": "cell",
+                        "in": "path",
+                        "required": true
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "ok",
+                        "schema": {
+                            "type": "string"
+                        }
+                    }
+                }
+            }
+        }
+    }
 }`
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
